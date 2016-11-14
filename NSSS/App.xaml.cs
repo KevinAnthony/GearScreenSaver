4e827589658ca74c301fac71c64d9c85e3ae9b82
echel0n <sickrage.tv@gmail.com>
2016-11-14 11:48:46 -0500

    Updated to Unity Engine
    Working for serveral months

--- NSSS/App.xaml.cs
+++ NSSS/App.xaml.cs
@@ -1,4 +1,7 @@
-﻿namespace NSSS
+﻿using System.Diagnostics;
+using Microsoft.Win32;
+
+namespace NSSS
 {
     #region
 
@@ -16,77 +19,40 @@
     /// </summary>
     public partial class App : Application
     {
-        #region Fields
 
-        private MainWindow screenSaver;
-
-        private HwndSource winWPFContent;
-
-        #endregion
 
         #region Methods
 
         private void ApplicationStartup(object sender, StartupEventArgs e)
         {
+            string filelocation = Registry.GetValue(@"HKEY_CURRENT_USER\SOFTWARE\NoSide\ScreenSaver", "nsss_path", string.Empty) as string;
+            if (filelocation == null) return;
             if (e.Args.Length > 0) {
                 // Preview mode--display in little window in Screen Saver dialog
                 // (Not invoked with Preview button, which runs Screen Saver in
                 // normal /s mode).
                 if (e.Args[0].ToLower().StartsWith("/p")) {
-                    this.screenSaver = new MainWindow();
-
-                    Int32 previewHandle = Convert.ToInt32(e.Args[1]);
-                    //WindowInteropHelper interopWin1 = new WindowInteropHelper(win);
-                    //interopWin1.Owner = new IntPtr(previewHandle);
-
-                    IntPtr pPreviewHnd = new IntPtr(previewHandle);
-
-                    Win32Interop.RECT lpRect = new Win32Interop.RECT();
-                    bool bGetRect = Win32Interop.Win32API.GetClientRect(pPreviewHnd, ref lpRect);
-
-                    HwndSourceParameters sourceParameters = new HwndSourceParameters("sourceParams");
-
-                    sourceParameters.PositionX = 0;
-                    sourceParameters.PositionY = 0;
-                    sourceParameters.Height = lpRect.Bottom - lpRect.Top;
-                    sourceParameters.Width = lpRect.Right - lpRect.Left;
-                    sourceParameters.ParentWindow = pPreviewHnd;
-                    sourceParameters.WindowStyle = (int)(Win32Interop.WindowStyles.WS_VISIBLE | Win32Interop.WindowStyles.WS_CHILD | Win32Interop.WindowStyles.WS_CLIPCHILDREN);
-
-                    this.winWPFContent = new HwndSource(sourceParameters);
-                    this.winWPFContent.Disposed += this.WinWpfContentDisposed;
-                    this.winWPFContent.RootVisual = this.screenSaver.ViewPort;
+                    Process notePad = new Process();
+                    notePad.StartInfo.FileName = filelocation;
+                    notePad.StartInfo.Arguments = "-multidisplay";
+                    notePad.Start();
                     return;
                 }
 
                 // Normal screensaver mode.  Either screen saver kicked in normally,
                 // or was launched from Preview button
                 else if (e.Args[0].ToLower().StartsWith("/s")) {
-                    foreach (Screen screen in Screen.AllScreens) {
-                        //creates a form just for that screen 
-                        //and passes it the bounds of that screen
-                        MainWindow screensaver = new MainWindow(screen.Bounds);
-                        screensaver.Show();
-                    }
+                    Process notePad = new Process();
+                    notePad.StartInfo.FileName = filelocation;
+                    notePad.StartInfo.Arguments = "-multidisplay";
+                    notePad.Start();
                     return;
                 }
 
                 // Config mode, launched from Settings button in screen saver dialog
                 else if (e.Args[0].ToLower().StartsWith("/c")) {}
             }
-            // If not running in one of the sanctioned modes, shut down the app
-            // immediately (because we don't have a GUI).
-            foreach (Screen screen in Screen.AllScreens)
-            {
-                MainWindow screensaver = new MainWindow(screen.Bounds);
-                screensaver.Show();
-            }
 
-        }
-
-        void WinWpfContentDisposed(object sender, EventArgs e)
-        {
-            this.screenSaver.Close();
         }
 
         #endregion
---
+++ NSSS_Unity/Assets/nsss.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace Assets
+{
+    public class nsss : MonoBehaviour {
+        
+        // Use this for initialization
+        public void Start () {
+            Debug.Log("Displays connected: " + Display.displays.Length);
+            foreach (var display in Display.displays) {
+                display.Activate(display.systemWidth, display.systemHeight, 60);
+            }
+        }
+
+        // Update is called once per frame
+        public void Update () {
+            if (Input.anyKey || !Input.GetAxis("Mouse X").Equals(0.0f) || !Input.GetAxis("Mouse Y").Equals(0.0f))
+            {
+                Application.Quit();
+            }
+            this.transform.Rotate(0, 90 * Time.deltaTime, 0);
+        }
+    }
+}
